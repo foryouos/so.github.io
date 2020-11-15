b8e3830823d421f593ac122028c747f66bb2252b
unknown <yuqianyi5211314@gmail.com>
2020-11-15 14:17:49 +0800

    双搜索引擎

--- data_devise.js
+++ data_devise.js
@@ -1,1 +1,19 @@
+window.onload =function (){
+    document.getElementById("google").onclick =so1;
 
+    document.getElementById("baidu").onclick =so2;
+}
+
+
+
+function so1()
+    {
+      document.getElementById("kw").name="q";
+      document.soform.action="https://g.teareading.app/search?";
+      document.soform.submit();
+    }
+function so2()
+    {
+      document.soform.action="https://www.baidu.com/s?";
+      document.soform.submit();
+    }
