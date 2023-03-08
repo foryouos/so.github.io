a781201adbebf5d090cb13c21eda2be03f2aae50
yuheshui <foryouos@qq.com>
2023-03-08 20:52:14 +0800

    浏览器定位暂时告一段落，使用固定新乡定位

--- index.js
+++ index.js
@@ -123,9 +123,94 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // 修改和风天气
+// 使用百度API获取位置
 
 
+//城市id
+loc_id = "0";  //初始值是新乡
+var searchZh = "河南";
+var searchShi = "新乡";
+var searchXian = "新乡";
 
+// 获取天气ID
+fetch("city.json")
+    .then(function (response) {
+      if(response.status === 200){
+        console.log("读取城市数据成功")
+        return response.json();
+      }
+      else{
+        console.log("读取城市地址数据错误")
+      }
+      })
+
+    .then(data=>{
+      // 循环省份
+      for(let i = 0;i<  data.length;i++)
+      {
+        if(data[i].zh === searchZh )
+        {
+          console.log(data[i]);
+          for(let j = 0; j < data[i].children.length;j++)
+          {
+             if(data[i].children[j].zh === searchShi)
+               {
+                 console.log(data[i].children[j]);
+                 for(let k = 0 ; k < data[i].children[j].children.length; k++ )
+                 {
+                     if(data[i].children[j].children[k].zh === searchXian)
+                     {
+                        console.log("县数据找到成功")
+                        // console.log(data[i].children[j].children[k]);
+                        // console.log("CN"+ data[i].children[j].children[k].id);
+                        // return data[i].children[j].children[k].id
+                        var loc_id=String("CN"+data[i].children[j].children[k].id);
+                        console.log(String(loc_id));
+                     }
+                     else {
+                       console.log(k);
+                     }
+                 }
+               }
+             // else{
+             //   console.log(j);
+             // }
+          }
+
+          break;
+        }
+        // else{
+        //   console.log(i)
+        // }
+      }
+
+    console.log(String(loc_id));
+    WIDGET = {
+          "CONFIG": {
+            "modules": "01234",
+            "background": "5",
+            "tmpColor": "E6B8AF",
+            "tmpSize": "16",
+            "cityColor": "434343",
+            "citySize": "16",
+            "aqiColor": "FF9900",
+            "aqiSize": "16",
+            "weatherIconSize": "18",
+            "alertIconSize": "18",
+            "padding": "10px 10px 10px 10px",
+            "shadow": "1",
+            "language": "auto",
+            "borderRadius": "10",
+            "fixed": "true",
+            "vertical": "center",
+            "horizontal": "left",
+            "city": String(loc_id),
+            "left": "40",
+            "top": "20",
+            "key": "6cfd3807500d4af38572be5b6b6d0f24"
+          }
+        }
+    })
 
 
 
